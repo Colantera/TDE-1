4d77be8041a080bdf62d8f6e37114e77bc5bcfa3
Pedro Henrique <ph.colantonio@gmail.com>
2025-09-16 19:18:25 -0300

    Add classe livros, construtor get

---
+++ src/Livro.java
@@ -0,0 +1,33 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int ano;
+    private String isbn;
+    private int quantidade;
+
+    public Livro(String titulo, String autor, int ano, String isbn, int quantidade) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.isbn = isbn;
+        this.quantidade = quantidade;
+    }
+
+    public boolean isDisponivel() {
+        return quantidade > 0;
+    }
+
+    public void emprestar() {
+        if (isDisponivel()) {
+            quantidade--;
+        }
+    }
+
+    public void devolver() {
+        quantidade++;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+}
