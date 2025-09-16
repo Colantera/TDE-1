4c936e38b4245e04b6a372ccdc62f4faf99dc829
Pedro Henrique <ph.colantonio@gmail.com>
2025-09-16 19:26:20 -0300

    add construtor e erro no usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,2 +1,14 @@
 public class Usuario {
+    protected String nome;
+    protected String matricula;
+
+    public Usuario(String nome, String matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public int prazoDevolucao() {
+        System.out.println("ERRO: O tipo de usuário não foi definido!");
+        return 0;
+    }
 }
