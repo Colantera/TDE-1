fe346c09f7806743980970d239cee827814fb92c
Pedro Henrique <ph.colantonio@gmail.com>
2025-09-16 19:34:10 -0300

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Biblioteca b = new Biblioteca();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Livro l1 = new Livro("Java Básico", "Autor A", 2020, "12345", 2);
+        Livro l2 = new Livro("POO em Prática", "Autor B", 2022, "67890", 1);
+
+        b.cadastrarLivro(l1);
+        b.cadastrarLivro(l2);
+
+        Usuario u1 = new Aluno("Pedro", "A001");
+        Usuario u2 = new Professor("Maria", "P001");
+
+        b.cadastrarUsuario(u1);
+        b.cadastrarUsuario(u2);
+
+        b.listarLivros();
+
+        b.emprestarLivro(u1, l1);
+        b.emprestarLivro(u2, l2);
+
+        b.listarEmprestimos();
+        b.listarLivros();
     }
 }
