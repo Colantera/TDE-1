2edf024eff75f2079720bcef7cc7459ac312d38d
Pedro Henrique <ph.colantonio@gmail.com>
2025-09-16 19:29:00 -0300

    update

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,2 +1,10 @@
-public class Aluno {
+public class Aluno extends Usuario {
+    public Aluno(String nome, String matricula) {
+        super(nome, matricula);
+    }
+
+    @Override
+    public int prazoDevolucao() {
+        return 7; // 7 dias
+    }
 }
